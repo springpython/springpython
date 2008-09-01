77df766d7e3b60fdd4a75639aafd546f3e92d537
Greg Turnquist <gturnquist@vmware.com>
2008-09-01 21:55:26 +0000

    IN PROGRESS - issue SESPRINGPYTHONPY-50: Investigate integrating PyUnit testing with bamboo CI http://jira.springframework.org/browse/SESPRINGPYTHONPY-50
    Made changes per IM chat with Tony to fine tune make_release.py.
    
    git-svn-id: https://src.springframework.org/svn/se-springpython-py/trunk/springpython@65 ce8fead1-4192-4296-8608-a705134b927f

--- make_release.py
+++ make_release.py
@@ -15,42 +15,44 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+from datetime import datetime
 import os
 import sys
 from optparse import OptionParser
 
-"""This script is a shortcut to making the python distribution releases. This should be run from the folder it is in."""
+# Read in the properties file, and parse it into a dictionary
+properties = {}
+inputProperties = [property.split("=") for property in open("springpython.properties").readlines()
+                   if not (property.startswith("#") or property.strip() == "")]
+filter(properties.update, map((lambda prop: {prop[0]: prop[1]}), inputProperties))
 
-# This is the base version of this release.
-version = "0.6.0"
+version = properties["version"]
 
 parser = OptionParser(usage="usage: %prog [-h|--help] [options]")
-parser.add_option("-c", "--clean", action="store_true", dest="clean", default=False, help="Clean out current target build.")
-parser.add_option("-b", "--build", action="store_true", dest="package", default=False, help="Same as the package option.")
-parser.add_option("-v", "--version", action="store", dest="version", default="", help="For --package, this specifies a special tag, generate version tag %s-<version>" % version)
-parser.add_option("-t", "--test", action="store_true", dest="test", default=False, help="Test everything, generating JUnit-style XML outputs.")
-parser.add_option("", "--package", action="store_true", dest="package", default=False, help="Package everything up into a tarball for release to sourceforge.")
-parser.add_option("", "--publish", action="store_true", dest="publish", default=False, help="Publish this release to the deployment server.")
-parser.add_option("-r", "--register", action="store_true", dest="register", default=False, help="Register this release with http://pypi.python.org/pypi")
+parser.add_option("-c", "--clean", action="store_true", dest="clean", default=False, help="clean out current target build.")
+parser.add_option("-b", "--build", action="store_true", dest="package", default=False, help="same as the package option.")
+parser.add_option("", "--build-stamp", action="store", dest="buildStamp", default="BUILD-%s" % datetime.now().strftime("%Y%m%d%H%M%S"), help="for --package, this specifies a special tag, generating version tag '%s-<build-stamp>'. Default: '%s-<current time>'." % (version, version))
+parser.add_option("-t", "--test", action="store_true", dest="test", default=False, help="test everything, generating JUnit-style XML outputs.")
+parser.add_option("", "--package", action="store_true", dest="package", default=False, help="package everything up into a tarball for release to sourceforge.")
+parser.add_option("", "--publish", action="store_true", dest="publish", default=False, help="publish this release to the deployment server.")
+parser.add_option("-r", "--register", action="store_true", dest="register", default=False, help="register this release with http://pypi.python.org/pypi")
 (options, args) = parser.parse_args()
 
-if options.version:
-    version += "-%s" % options.version
+completeVersion = version + "-" + options.buildStamp
 
 if options.clean:
     print "Cleaning out the target directory"
     os.system("rm -rf target")
             
 elif options.test:
-    os.system("mkdir -p target/test-results")
-    os.system("nosetests --with-nosexunit --source-folder=src --where=test/springpythontest --xml-report-folder=target/test-results")
+    os.system("mkdir -p target/test-results/xml")
+    os.system("nosetests --with-nosexunit --source-folder=src --where=test/springpythontest --xml-report-folder=target/test-results/xml")
 
 elif options.package:
-    os.system("mkdir -p target/test-results")
-    os.system("cd src ; python setup.py --version %s sdist ; mv dist/* .. ; \\rm -rf dist ; \\rm -f MANIFEST" % version)
-    os.system("cd samples ; python setup.py --version %s sdist ; mv dist/* .. ; \\rm -rf dist ; \\rm -f MANIFEST" % version)
-    os.system("mv *.tar.gz target")
+    os.system("mkdir -p target/artifacts")
+    os.system("cd src ; python setup.py --version %s sdist ; mv dist/* .. ; \\rm -rf dist ; \\rm -f MANIFEST" % completeVersion)
+    os.system("cd samples ; python setup.py --version %s sdist ; mv dist/* .. ; \\rm -rf dist ; \\rm -f MANIFEST" % completeVersion)
+    os.system("mv *.tar.gz target/artifacts")
 	
 elif options.publish:
     # TODO(8/28/2008 GLT): Implement automated solution for this.
@@ -58,6 +60,6 @@ elif options.publish:
 
 elif options.register:
     # TODO(8/28/2008 GLT): Test this part when making official release and registering to PyPI.
-	os.system("cd src ; python setup.py --version %s register" % version)
-	os.system("cd samples ; python setup.py --version %s register" % version)
+	os.system("cd src ; python setup.py --version %s register" % completeVersion)
+	os.system("cd samples ; python setup.py --version %s register" % completeVersion)
 
