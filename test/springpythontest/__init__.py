74f6306baed3538c3a6f5d419e5c226f898822e9
Greg Turnquist <gturnquist@vmware.com>
2008-09-04 17:08:20 +0000

    SESPRINGPYTHONPY-50: Disable sqlite test cases, to see if integration with bamboo works every where else.
    git-svn-id: https://src.springframework.org/svn/se-springpython-py/trunk/springpython@74 ce8fead1-4192-4296-8608-a705134b927f

--- test/springpythontest/__init__.py
+++ test/springpythontest/__init__.py
@@ -17,11 +17,11 @@ from springpythontest.aopTestCases import *
 from springpythontest.contextTestCases import *
 #from springpythontest.databaseCoreTestCases import MySQLDatabaseTemplateTestCase
 #from springpythontest.databaseCoreTestCases import PostGreSQLDatabaseTemplateTestCase
-from springpythontest.databaseCoreTestCases import SqliteDatabaseTemplateTestCase
+#from springpythontest.databaseCoreTestCases import SqliteDatabaseTemplateTestCase
 from springpythontest.databaseCoreTestCases import DatabaseTemplateMockTestCase
 #from springpythontest.databaseTransactionTestCases import MySQLTransactionTestCase
 #from springpythontest.databaseTransactionTestCases import PostGreSQLTransactionTestCase
-from springpythontest.databaseTransactionTestCases import SqliteTransactionTestCase
+#from springpythontest.databaseTransactionTestCases import SqliteTransactionTestCase
 from springpythontest.remotingTestCases import *
 from springpythontest.securityEncodingTestCases import *
 from springpythontest.securityProviderTestCases import *
