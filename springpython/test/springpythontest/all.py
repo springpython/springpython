b04b8abc4c2bf7341534072841d86c19a9042b33
Greg Turnquist <gturnquist@vmware.com>
2009-02-16 12:27:14 +0000

    Adjusting testing to handle multiple versions, like integration, unit test, and everything.
    git-svn-id: https://src.springframework.org/svn/se-springpython-py/trunk@469 ce8fead1-4192-4296-8608-a705134b927f

---
+++ springpython/test/springpythontest/all.py
@@ -0,0 +1,17 @@
+from springpythontest.aopTestCases import *
+from springpythontest.contextTestCases import *
+from springpythontest.databaseCoreTestCases import MySQLDatabaseTemplateTestCase
+from springpythontest.databaseCoreTestCases import PostGreSQLDatabaseTemplateTestCase
+from springpythontest.databaseCoreTestCases import SqliteDatabaseTemplateTestCase
+from springpythontest.databaseCoreTestCases import DatabaseTemplateMockTestCase
+from springpythontest.databaseTransactionTestCases import MySQLTransactionTestCase
+from springpythontest.databaseTransactionTestCases import PostGreSQLTransactionTestCase
+from springpythontest.databaseTransactionTestCases import SqliteTransactionTestCase
+from springpythontest.remotingTestCases import PyroRemotingTestCase
+from springpythontest.remotingTestCases import HessianRemotingTestCase
+from springpythontest.securityEncodingTestCases import *
+from springpythontest.securityProviderTestCases import *
+from springpythontest.securityUserDetailsTestCases import *
+from springpythontest.securityVoteTestCases import *
+from springpythontest.securityWebTestCases import *
+
