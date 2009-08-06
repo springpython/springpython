0a24ae911d9f536273a4e6ad22207d04eb01e445
Greg Turnquist <gturnquist@vmware.com>
2009-08-06 18:24:16 +0000

    Tagged M1 release of Spring Python 1.1.0.
    git-svn-id: https://src.springframework.org/svn/se-springpython-py/trunk@512 ce8fead1-4192-4296-8608-a705134b927f

--- springpython/samples/build.py
+++ springpython/samples/build.py
@@ -42,7 +42,7 @@ setup(name='springpython-samples',
       classifiers=["License :: OSI Approved :: GNU General Public License (GPL)",
                    "License :: OSI Approved :: Apache Software License",
                    "Intended Audience :: Developers",
-                   "Development Status :: 5 - Production/Stable",
+                   "Development Status :: 4 - Beta",
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    "Programming Language :: Python",
                    "Operating System :: OS Independent"
