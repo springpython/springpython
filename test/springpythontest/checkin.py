d384421f54030b8de1b94a4593e888c2013c4999
Greg Turnquist <gturnquist@vmware.com>
2009-09-04 16:05:06 +0000

    SESPRINGPYTHONPY-107: Tweaked files and made sure everything passes.
    git-svn-id: https://src.springframework.org/svn/se-springpython-py/trunk/springpython@535 ce8fead1-4192-4296-8608-a705134b927f

--- test/springpythontest/checkin.py
+++ test/springpythontest/checkin.py
@@ -1,29 +1,13 @@
 from springpythontest.aopTestCases import *
 from springpythontest.contextTestCases import *
-#from springpythontest.databaseCoreTestCases import MySQLDatabaseTemplateTestCase
-#from springpythontest.databaseCoreTestCases import PostGreSQLDatabaseTemplateTestCase
+from springpythontest.databaseCoreTestCases import ConnectionFactoryTestCase
 from springpythontest.databaseCoreTestCases import SqliteDatabaseTemplateTestCase
-#from springpythontest.databaseCoreTestCases import SQLServerDatabaseTemplateTestCase
 from springpythontest.databaseCoreTestCases import DatabaseTemplateMockTestCase
-#from springpythontest.databaseTransactionTestCases import MySQLTransactionTestCase
-#from springpythontest.databaseTransactionTestCases import PostGreSQLTransactionTestCase
 from springpythontest.databaseTransactionTestCases import SqliteTransactionTestCase
-#from springpythontest.databaseTransactionTestCases import SQLServerTransactionTestCase
-#from springpythontest.remotingTestCases import PyroRemotingTestCase
-#from springpythontest.remotingTestCases import HessianRemotingTestCase
 from springpythontest.securityEncodingTestCases import *
 from springpythontest.securityProviderTestCases import *
 from springpythontest.securityUserDetailsTestCases import *
 from springpythontest.securityVoteTestCases import *
 from springpythontest.securityWebTestCases import *
-
-logger = logging.getLogger("springpython")
-loggingLevel = logging.DEBUG
-logger.setLevel(loggingLevel)
-ch = logging.StreamHandler()
-ch.setLevel(loggingLevel)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
 
