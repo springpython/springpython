ce2a1aff8e7a810fb436bbc52050871df632ed3f
Greg Turnquist <greg.turnquist@springsource.com>
2010-06-29 14:53:44 -0500

    Added more tests to checkout YamlConfig.

--- test/springpythontest/unicodeTestCases.py
+++ test/springpythontest/unicodeTestCases.py
@@ -2,7 +2,7 @@ import unittest
 from springpython.context import *
 from springpython.config import *
 
-class UnicodeTestCase(unittest.TestCase):
+class UnicodeTestCaseXml(unittest.TestCase):
 	def testParsingAppContextWithNonAscii1(self):
 		ctx = ApplicationContext(XMLConfig("support/unicodeContext1.xml"))
 
@@ -33,6 +33,39 @@ class UnicodeTestCase(unittest.TestCase):
 	def testParsingAppContextWithNonAscii10(self):
  		try:
  			ctx = ApplicationContext(XMLConfig("support/unicodeContext10.xml"))
+			self.fail("Expected an encoding error since Python code is ASCII based.")
+		except UnicodeEncodeError:
+			pass
+
+class UnicodeTestCaseYaml(unittest.TestCase):
+	def testParsingAppContextWithNonAscii1(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext1.yaml"))
+
+	def testParsingAppContextWithNonAscii3(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext3.yaml"))
+
+	def testParsingAppContextWithNonAscii4(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext4.yaml"))
+
+	def testParsingAppContextWithNonAscii5(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext5.yaml"))
+
+
+	def testParsingAppContextWithNonAscii6(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext6.yaml"))
+
+	def testParsingAppContextWithNonAscii7(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext7.yaml"))
+
+	def testParsingAppContextWithNonAscii8(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext8.yaml"))
+
+	def testParsingAppContextWithNonAscii9(self):
+		ctx = ApplicationContext(YamlConfig("support/unicodeContext9.yaml"))
+
+	def testParsingAppContextWithNonAscii10(self):
+ 		try:
+ 			ctx = ApplicationContext(YamlConfig("support/unicodeContext10.yaml"))
 			self.fail("Expected an encoding error since Python code is ASCII based.")
 		except UnicodeEncodeError:
 			pass
