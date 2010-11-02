cfd6e1101ec5d048127237b092c5861276667f2c
Greg Turnquist <greg.turnquist@springsource.com>
2010-11-02 17:10:26 -0500

    Added test.bash for CI

---
+++ test.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+###################################################################################
+#   Copyright 2006-2008 SpringSource (http://springsource.com), All Rights Reserved
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.       
+###################################################################################
+# This script is used to support continuous integration.
+#
+# NOTE: Do NOT alter this file without explicit permission from Project Lead
+#       Greg Turnquist.
+###################################################################################
+
+# For testing locally
+#. /Users/gturnquist/sp/bin/activate
+
+# For bamboo
+. /home/bamboo/sp/bin/activate
+
+python build.py --clean  --test
+
